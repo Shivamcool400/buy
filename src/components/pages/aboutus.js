010218e54794edadfafda543730be0e65c776587
Goyal2001-30 <66236007+Goyal2001-30@users.noreply.github.com>
2021-03-22 17:18:27 +0530

    Minor Changes in about.js and image styling

--- src/components/pages/aboutus.js
+++ src/components/pages/aboutus.js
@@ -5,33 +5,33 @@ class Aboutus extends Component{
     render() {
         return(
         
-            <div class="body">
-            <h1 class="heading">About Us</h1>
-            <div class="box">
+            <div className="body">
+            <h1 className="heading">About Us</h1>
+            <div className="box">
       
-              <div class="item1">
+              <div className="item1 shadow p-3 mb-5 bg-white rounded">
                 <div>
-                  <img src="/ourvision.jpg" alt="Our Vision" class="image" />
+                  <img src="/ourvision.jpg" alt="Our Vision" className="image1" />
                 </div>
-                <div class="para1">
+                <div className="para1">
                   <h3>Our Vision</h3>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum laudantium eos, dolorem soluta facilis officiis culpa excepturi atque sunt consectetur recusandae eveniet doloremque corporis labore aliquam inventore in quis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim repudiandae iusto quis. Omnis voluptate quidem sint veniam, dolorem aliquam accusantium totam. Reprehenderit dolores facilis beatae tenetur, autem perspiciatis amet temporibus?</p>
                 </div>
               </div>
       
-              <div class="item2">
+              <div className="item2 shadow p-3 mb-5 bg-white rounded">
                 <div>
-                  <img src="/ourapproach.jpg" alt="Our Approach" class="image" /></div>
-                <div class="para2">
+                  <img src="/ourapproach.jpg" alt="Our Approach" className="image2" /></div>
+                <div className="para2">
                   <h3>Our Approach</h3>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum laudantium eos, dolorem soluta facilis officiis culpa excepturi atque sunt consectetur recusandae eveniet doloremque corporis labore aliquam inventore in quis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim repudiandae iusto quis. Omnis voluptate quidem sint veniam, dolorem aliquam accusantium totam. Reprehenderit dolores facilis beatae tenetur, autem perspiciatis amet temporibus?</p>
                 </div>
               </div>
       
-              <div class="item3">
+              <div className="item3 shadow p-3 mb-5 bg-white rounded">
                 <div>
-                  <img src="/ourprocess.jpg" alt="Our Process" class="image" /></div>
-                <div class="para3">
+                  <img src="/ourprocess.jpg" alt="Our Process" className="image3" /></div>
+                <div className="para3">
                   <h3>Our Process</h3>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum laudantium eos, dolorem soluta facilis officiis culpa excepturi atque sunt consectetur recusandae eveniet doloremque corporis labore aliquam inventore in quis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim repudiandae iusto quis. Omnis voluptate quidem sint veniam, dolorem aliquam accusantium totam. Reprehenderit dolores facilis beatae tenetur, autem perspiciatis amet temporibus?</p>
                 </div>
