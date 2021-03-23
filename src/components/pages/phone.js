fb38ea3e2d6ba0bba562be0db5904bffe7d5ba73
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-03-24 01:45:01 +0530

    started working on phone section. edit (2)

--- src/components/pages/phone.js
+++ src/components/pages/phone.js
@@ -4,8 +4,111 @@ import './phone.css';
 class Phones extends Component{
     render() {
         return(
-        
-        <p> work in progress</p>
+            <div>
+        <div className="container" className="back">
+            <figure className="text-center ">
+      <blockquote className="blockquote ">
+        <h2 className="msg-heading">Find Your Device ?</h2>
+      </blockquote>
+    </figure>
+    <br></br>
+    <br></br>
+    <div className="row">
+           <div className="col-sm-12 col-md-3"><div className="input-group mb-3">
+  <label className="input-group-text" htmlFor="inputGroupSelect01">Brand</label>
+  <select className="form-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <option value={1}>One-plus</option>
+    <option value={2}>Samsung</option>
+    <option value={3}>Apple</option>
+  </select>
+</div>
+</div>
+           <div className="col-sm-12 col-md-3"><div className="input-group mb-3">
+  <label className="input-group-text" htmlFor="inputGroupSelect01">Price-range</label>
+  <select className="form-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <option value={1}>below-10000</option>
+    <option value={2}>below-20000</option>
+    <option value={3}>above-30000</option>
+  </select>
+</div>
+</div>
+           <div className="col-sm-12 col-md-3"><div className="input-group mb-3">
+  <label className="input-group-text" htmlFor="inputGroupSelect01">Genere</label>
+  <select className="form-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <option value={1}>Normal</option>
+    <option value={2}>Gaming</option>
+    <option value={3}>Speedy</option>
+  </select>
+</div>
+</div>
+           <div className="col-sm-12 col-md-3"><div className="input-group mb-3">
+  <label className="input-group-text" htmlFor="inputGroupSelect01">Ram/Storage</label>
+  <select className="form-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <option value={1}>4gb above 64gb storage</option>
+    <option value={2}>6gb above 64gb storage</option>
+    <option value={3}>8gb above 64gb storage</option>
+  </select>
+</div>
+</div>
+</div>
+<div className="">
+            <button type="button" className="btn  btn-primary btn-outline-secondary btn-lg find-btn"> Find! </button>
+          </div>
+          <br></br>
+          <br></br>
+</div>
+
+<figure className="text-center ">
+      <blockquote className="blockquote ">
+        <h2 className="msg-heading text-dark"># Brands We Operate</h2>
+      </blockquote>
+    </figure>
+        <div className="row">
+          <div className="col-sm-12 col-md-4">
+          <img src="/oppo.jpg" className="rounded mx-auto d-block w-100 h-75" alt="..." />
+
+          </div>
+          <div className="col-sm-12 col-md-4">
+          <img src="/samsung.png" className="rounded mx-auto d-block w-100 h-75" alt="..." />
+          </div>
+          <div className="col-sm-12 col-md-4">
+          <img src="/apple.png" className="rounded mx-auto d-block w-100 h-75" alt="..." />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12 col-md-4">
+          <img src="/mi.jpg" className="rounded mx-auto d-block w-100 h-75" alt="..." />
+          </div>
+          <div className="col-sm-12 col-md-4">
+          <img src="/oneplus.jpg" className="rounded mx-auto d-block w-100 h-75" alt="..." />
+          </div>
+          <div className="col-sm-12 col-md-4">
+          <img src="/micromax.png" className="rounded mx-auto d-block w-100 h-75" alt="..." />
+          </div>
+        </div>
+
+        <br></br>
+        <br></br>
+
+        <div className="container" className="back-2">
+            
+        <figure className="text-center ">
+      <blockquote className="blockquote ">
+        <h2 className="msg-heading text-dark">BUY EXPERTS</h2>
+      </blockquote>
+    </figure>
+            
+
+        </div>
+
+        </div>
+         
+       
         
         
         ); } }
