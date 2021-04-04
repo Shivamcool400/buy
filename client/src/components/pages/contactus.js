1281d3d3a677ef611823b6ec31b2d4100b568381
Goyal2001-30 <66236007+Goyal2001-30@users.noreply.github.com>
2021-04-04 12:40:53 +0530

    Contact us backend done

--- client/src/components/pages/contactus.js
+++ client/src/components/pages/contactus.js
@@ -2,6 +2,33 @@ import React, {Component} from 'react';
 import './contactus.css'
 
 class Contactus extends Component{
+
+    state = {
+        name: '',
+        email: '',
+        message: '',
+        responseToPost: ''
+    }
+
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/contactus', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+              name: this.state.name,
+              email: this.state.email,
+              message: this.state.message
+            }),
+        });
+        const body = await response.text();
+        
+        this.setState({ responseToPost: 'Your message has been sent successfully.' });
+      };
+
     render() {
         return(
         
@@ -9,29 +36,34 @@ class Contactus extends Component{
             <h1 className="head"> Contact Us</h1>
             <div className="box box2">
                 <div className="left">
-                    <form method="" action="/contactus">
+                    <form method="POST" action="/contactus" onSubmit={this.handleSubmit}>
                         <fieldset>
                             <legend>Get in touch</legend>
+                            <p className="text-success text-center">{this.state.responseToPost}</p>
                             <div class="form-group input">
-                                <input type="text" class="form-control" name="name" placeholder="Enter name" />
+                                <input type="text" class="form-control" name="name" placeholder="Enter name" value={this.state.name} onChange={e => {this.setState({name: e.target.value})}} />
                             </div>
                             <div class="form-group input">
-                                <input type="email" class="form-control" name="email" placeholder="Enter email" />
+                                <input type="email" class="form-control" name="email" placeholder="Enter email" value={this.state.email} onChange={e => {this.setState({email: e.target.value})}} />
                                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div class="form-group input">
-                                <textarea class="form-control" rows="5" id="message" placeholder="Enter your message here..."></textarea>
+                                <textarea class="form-control" rows="5" id="message" placeholder="Enter your message here..." name="message" value={this.state.message} onChange={e => {this.setState({message: e.target.value})}} ></textarea>
                             </div>
+                            
                             <button className="btn btnmessage">Submit</button>
+                            
                         </fieldset>
                     </form>
                 </div>
+
+                
 
                 <div className="right">
                     <div className="emailicon shadow p-3 mb-5 bg-white rounded">
                         <em>For more queries, mail at</em>
                         <i className="fa fa-envelope-o fa-3x"></i>
-                        <a href="#" className="email">buyexpert400@gmail.com</a>
+                        <a href="https://www.gmail.com mailto: shivam.ece.pu@gmail.com" className="email">shivam.ece.pu@gmail.com</a>
                     </div>
                 </div>
             </div>
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const User = require('../models/models');
+const User = require('./models/models');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-mongoose.connect("mongodb+srv://webexpert:webexpert@cluster0.npqgi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true})
+
+mongoose.connect("mongodb+srv://new_user31:shashi1967@buyexperts.wdiuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true})
 .then(() => console.log('Database connected'))
 .catch((err) => console.log(err));
 
 
 app.post('/contactus', (req, res) => {
     const {name, email, message} = req.body;
+    User({
+        name,
+        email,
+        message
+    }).save((err,data) => {
+        if(err) throw err;
+
+    })
 });
 
 
